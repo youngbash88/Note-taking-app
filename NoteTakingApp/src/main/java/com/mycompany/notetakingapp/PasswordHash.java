3a06585f3e74dc54f26511f3dacf54d4b86acc87
Youssef Gamal <youssefgamal6665@gmail.com>
2024-12-16 16:30:39 +0200

    added all the important updates

---
+++ NoteTakingApp/src/main/java/com/mycompany/notetakingapp/PasswordHash.java
@@ -0,0 +1,22 @@
+package com.mycompany.notetakingapp;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class PasswordHash {
+
+    public static String hashPassword(String password) {
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] hashBytes = digest.digest(password.getBytes());
+            StringBuilder hexString = new StringBuilder();
+            for (byte b : hashBytes) {
+                hexString.append(String.format("%02x", b));
+            }
+            return hexString.toString();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
--- NoteTakingApp/src/main/java/com/mycompany/notetakingapp/SecureNote.java
+++ NoteTakingApp/src/main/java/com/mycompany/notetakingapp/SecureNote.java
@@ -1,13 +1,21 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.notetakingapp;
 
-/**
- *
- * @author DELL
- */
-public class SecureNote {
-    
+import java.io.Serializable;
+
+public class SecureNote extends Note implements Serializable {
+    private String password;
+
+    // Constructor for SecureNote
+    public SecureNote(String title, String content, String imagePath, String sketchPath, String password) {
+        super(title, content, imagePath, sketchPath);  // Call the parent constructor (Note class)
+        this.password = password;
+    }
+
+    // Getter for password
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
